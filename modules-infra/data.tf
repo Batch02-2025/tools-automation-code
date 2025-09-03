6f8c82b9db32859f0cb6bbd53e65c4bdae3acbbd
MannuA19 <mannmeet.ahir@gmail.com>
2025-09-03 15:52:55 +0530

    Terrafrom Code Updated

--- modules-infra/data.tf
+++ modules-infra/data.tf
@@ -1,5 +1,5 @@
-data "aws_ami" "rhel9" {
-  most_recent     = true
-  name_regex      = "Red Hat Enterprise Linux 9 (HVM), SSD Volume Type"
-  owners          = ["959620655822"]
-}
+# data "aws_ami" "rhel9" {
+#   most_recent     = true
+#   name_regex      = "Red Hat Enterprise Linux 9 (HVM), SSD Volume Type"
+#   owners          = ["959620655822"]
+# }
