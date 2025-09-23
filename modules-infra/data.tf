5dcf89093426e66d53aa217102acb65391c10286
MannuA19 <mannmeet.ahir@gmail.com>
2025-09-23 16:17:59 +0530

    Terrafrom Code Updated

--- modules-infra/data.tf
+++ modules-infra/data.tf
@@ -1,14 +1,28 @@
 data "aws_ami" "rhel9" {
-  most_recent     = true
-  name_regex      = "Red Hat Enterprise Linux 9 (HVM), SSD Volume Type"
-  architecture    = "x86_64"
-  owners          = ["959620655822"]
+  most_recent = true
+  owners      = ["959620655822"]
 
   filter {
-    name = "Red Hat Enterprise Linux version 9 (HVM), EBS General Purpose (SSD) Volume Type"
-    values = ["ami-*"]
+    name   = "name"
+    values = ["Red Hat Enterprise Linux 9 (HVM), SSD Volume Type*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
+
 
 data "aws_key_pair" "key" {
   key_name = var.key_name
